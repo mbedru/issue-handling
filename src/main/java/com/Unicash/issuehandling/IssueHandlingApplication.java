fa6cf05e9f17f61a51095be9cfbe412ee15ec80a
mame <mbedru18@yahoo.com>
2023-03-01 15:59:36 +0300

    Our first commit !!

---
+++ src/main/java/com/Unicash/issuehandling/IssueHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.Unicash.issuehandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Unicash/issuehandling/IssueHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unicash.issuehandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
